85b2ce36c0a7086e81bcd2617691521f1ac984ca
Dung Tran <pdung1989@gmail.com>
2020-07-15 20:51:42 +0300

    alert/prompt basic

---
+++ Javascript/scriptexercise/scriptexercise.js
@@ -0,0 +1,12 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("How old are you?");
+
+
+var fullName = firstName + " " + lastName
+console.log("My full name is " + fullName);
+
+// console.log("My name is " + firstName + " " + lastName);
+
+console.log("I am " + age + " years old.");
+
